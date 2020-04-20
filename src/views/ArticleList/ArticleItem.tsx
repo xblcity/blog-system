1489af85098063e0d6688ae5247a954bb4660807
xblcity <hyaxie@163.com>
2020-04-20 10:09:10 +0800

    feat: 完成文章列表

--- src/views/ArticleList/ArticleItem.tsx
+++ src/views/ArticleList/ArticleItem.tsx
@@ -43,7 +43,91 @@ interface IProps {
 }
 
 const ArticleItem = ({ data, getTargetArticleId }: IProps) => {
-  return <div>12313</div>
+  const { title, content, id, tags, comments } = data
+
+  const windowWidth = getWindowWidth()
+
+  const {
+    state: { tagList }
+  } = useTagStore()
+
+  const tempTagList = getTagColor(tagList, tags)
+
+  const discussCount = useMemo(() => {
+    let count = comments.length
+    comments.forEach(item => {
+      count = count + item.replies.length
+    })
+    return count
+  }, [comments])
+
+  return (
+    <div className={styles.articleItem}>
+      {windowWidth > 768 ? (
+        <Divider orientation="left">
+          <span onClick={() => getTargetArticleId(id)} className={styles.title}>
+            {title}
+          </span>
+          <span className={styles.createTime}>{data.createdAt}</span>
+        </Divider>
+      ) : (
+        <div className={styles.mobileTitleWrapper}>
+          <div onClick={() => getTargetArticleId(id)} className={styles.title}>
+            {title}
+          </div>
+          <div className={styles.createTime}>{data.createdAt}</div>
+        </div>
+      )}
+
+      <div
+        className={classnames(styles.description, styles.markdown)}
+        dangerouslySetInnerHTML={{ __html: markdownToHtml(content) }}
+        onClick={() => getTargetArticleId(id)}
+      />
+      <div className={styles.otherInfo}>
+        <div className={styles.viewCountAndDicuss}>
+          <div className={styles.item}>
+            <Icon
+              className={styles.icon}
+              width={16}
+              height={16}
+              color="#828a8c"
+              id="discuss2e"
+            />
+            {discussCount}
+          </div>
+          <div className={styles.item}>
+            <Icon
+              className={styles.icon}
+              width={20}
+              height={20}
+              color="#828a8c"
+              id="yanjing"
+            />
+            {data.viewCount}
+          </div>
+        </div>
+        {!!tags.length && windowWidth > 768 && (
+          <>
+            <Divider type="vertical" />
+            <div className={styles.tagContainer}>
+              <Icon
+                width={16}
+                height={16}
+                className={styles.tagIcon}
+                color="#838a8c"
+                id="tags"
+              />
+              <ArticleTags tags={tempTagList} />
+            </div>
+          </>
+        )}
+      </div>
+      {windowWidth <= 768 && (
+        <ArticleTags className={styles.mobildTags} tags={tempTagList} />
+      )}
+    </div>
+  )
 }
 
 export default ArticleItem
--- src/views/ArticleList/index.tsx
+++ src/views/ArticleList/index.tsx
@@ -1,13 +1,149 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Pagination, Empty, Icon, Drawer } from 'antd'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
 
-// interface SearchQuery {
-//   page: number
-//   keyword?: string
-// }
+import styles from './index.scss'
+import PageLoading from '@components/PageLoading'
+import Article, { ArticleItem } from './ArticleItem'
+import ListPreview from './ListPreview'
+import { decodeQuery, getWindowWidth } from '@utils/index'
+import { useGetListData } from '@utils/hooks'
+import { getArticleList } from '@services/api'
+
+interface SearchQuery {
+  page: number
+  keyword?: string
+}
+
+const pageSize = 10
 
 const ArticleList = ({ history, location }: RouteComponentProps) => {
-  return <div>12313</div>
+  const [params, setParams] = useState<FetchParams.GetArticleList>(null)
+  const [cancelRequire, setCancelRequire] = useState<boolean>(true)
+
+  // 移动端抽屉展示
+  // eslint-disable-next-line prettier/prettier
+  const [isShowTitleListDrawer, setIsShowTitleListDrawer] = useState<boolean>(false)
+
+  const windowWidth = getWindowWidth()
+
+  const { list, loading, total } = useGetListData<
+    ArticleItem,
+    FetchParams.GetArticleList
+  >(getArticleList, params, cancelRequire)
+
+  const getList = async (page: number, keyword?: string) => {
+    const data = {
+      page,
+      pageSize,
+      keyword,
+      isContentLimit: true
+    }
+    setCancelRequire(false)
+    setParams(data)
+  }
+
+  // 跳转文章详情
+  const getTargetArticleId = (id: number) => {
+    history.push(`/article-detail/${id}`)
+    setIsShowTitleListDrawer(false)
+  }
+
+  // 改变页码
+  const changePage = (p: number) => {
+    const urlParams = decodeQuery(location.search)
+    const params: PlainObj & SearchQuery = !!location.search
+      ? { ...urlParams, page: p }
+      : { page: p }
+    let url: string
+    Object.keys(params).forEach(key => {
+      url = !url ? `?${key}=${params[key]}` : `${url}&${key}=${params[key]}`
+    })
+    history.push(url)
+  }
+
+  useEffect(() => {
+    const { page, keyword } = decodeQuery(location.search)
+    getList(!!page ? page : 1, keyword)
+  }, [location.search])
+
+  const { page, keyword } = decodeQuery(location.search)
+
+  return (
+    <div className={styles.article}>
+      <div className={styles.articleListWrapper}>
+        {loading ? (
+          <PageLoading />
+        ) : (
+          <>
+            {!!list.length ? (
+              <div className={styles.articleListContainer}>
+                <div className={styles.articleList}>
+                  {list.map(item => (
+                    <Article
+                      getTargetArticleId={getTargetArticleId}
+                      key={item.id}
+                      data={item}
+                    />
+                  ))}
+                </div>
+                <ListPreview
+                  getTargetArticleId={getTargetArticleId}
+                  list={list}
+                />
+              </div>
+            ) : (
+              <Empty
+                description={
+                  !!keyword
+                    ? `未找到标题含有 ${keyword} 的文章`
+                    : '暂时还没有文章呢'
+                }
+                className={styles.empty}
+              />
+            )}
+          </>
+        )}
+      </div>
+      {!!list.length && (
+        <div className={styles.pagination}>
+          <Pagination
+            pageSize={pageSize}
+            onChange={changePage}
+            total={total}
+            current={!!page ? Number(page) : 1}
+          />
+        </div>
+      )}
+      {windowWidth <= 576 && (
+        <>
+          <div
+            onClick={() => setIsShowTitleListDrawer(true)}
+            className={styles.mobileDrawerBtn}
+          >
+            <Icon type="menu-o" />
+          </div>
+          <Drawer
+            className={styles.mobileDrawer}
+            closable={false}
+            title="文章列表"
+            onClose={() => setIsShowTitleListDrawer(false)}
+            visible={isShowTitleListDrawer}
+          >
+            {list.map(item => (
+              <div
+                onClick={() => getTargetArticleId(item.id)}
+                className={styles.mobileArticleTitle}
+                key={item.id}
+              >
+                {item.title}
+              </div>
+            ))}
+          </Drawer>
+        </>
+      )}
+    </div>
+  )
 }
 
 export default withRouter(ArticleList)
